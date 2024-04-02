e672688beb0c6004951f71262df70fe61a43fc81
Diego Guimaraes <diego.sena.guimaraes@gmail.com>
2024-04-02 20:11:32 -0300

    changes

---
+++ cartesify_backend/App.py
@@ -0,0 +1,64 @@
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+class AppOptions:
+
+    def __init__(self, url: str, broadcast_advance_requests: bool):
+        self.url = url
+        self.broadcast_advance_requests = broadcast_advance_requests
+
+
+class App:
+
+    def __init__(self, options: AppOptions):
+        self.options = options
+        self.advance_handlers = []
+        self.inspect_handlers = []
+
+    async def handle_advance(self, data):
+        print("handle_advance", data)
+        result = self.advance_handlers[0](data)
+        return result
+
+    async def handle_inspect(self, data):
+        print("handle_inspect", data)
+        return self.inspect_handlers[0](data)
+
+    def add_advance_handler(self, handler):
+        self.advance_handlers.append(handler)
+
+    def add_inspect_handler(self, handler):
+        self.inspect_handlers.append(handler)
+
+    async def start(self):
+        logger.info("App initiating")
+        status = "accept"
+        while True:
+            try:
+
+                response = requests.post(
+                      f"{self.options.url}/finish",
+                       json={"status": status},
+                       headers={"Content-Type": "application/json"}
+                )
+
+                if response.status_code == 200:
+                    data = response.json()
+
+                    if data['request_type'] == 'advance_state':
+                        status = await self.handle_advance(data['data'])
+                        break
+
+                    if data['request_type'] == 'advance_state':
+                        await self.handle_inspect(data['data'])
+                        break
+
+                elif response.status_code == 202:
+                    print("No rollup request available")
+            except Exception as e:
+                logger.info(f'Erro ao iniciar cartesify {e}')
+                print(e)
--- cartesify_backend/cartesifybackend.py
+++ cartesify_backend/cartesifybackend.py
@@ -2,35 +2,43 @@ import json
 import requests
 import logging
 from .appfactory import AppFactory
-from cartesi import Rollup, RollupData
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-def dapp_inspect_decorator(func):
-    def wrapper(self, *args, **kwargs):
-        return self.dapp.inspect()
-    return wrapper
+class CartesifyOptions:
 
-def dapp_advance_decorator(func):
-    def wrapper(self, *args, **kwargs):
-        return self.dapp.advance()
-    return wrapper
+    def __init__(self, url: str, broadcast_advance_requests: bool):
+        self.url = url
+        self.broadcast_advance_requests = broadcast_advance_requests
+
+    def __str__(self):
+        return f'CartesifyOptions: url={self.url}, broadcast_advance_requests={self.broadcast_advance_requests}'
 
 class CartesifyBackend:
 
-    def __init__(self):
-        logger.info("Starting Cartesify App")
-        self.dapp = AppFactory().create_dapp()
-    def get_app(self):
-        return self.dapp
+    def create_app(self, options: CartesifyOptions):
+        factory = AppFactory()
+        app = factory.create_dapp(options)
+        logger.info("App created")
 
-    @dapp_inspect_decorator
-    def handle_inspect(self, rollup:Rollup, data: RollupData) -> bool:
+        app.add_advance_handler(self.handle_advance)
+
+        logger.info("Advance Handler added")
+
+        app.add_inspect_handler(self.handle_inspect)
+
+        logger.info("Inspect Handler added")
+
+        return app
+
+
+    def handle_inspect(self, payload):
         logger.info("Cartesify handle inspect")
         try:
-            payload = data.str_payload()
+            if not payload.startswith('0x7b22'):
+                return "reject"
 
             hex_string = payload.replace('0x', '')
             byte_buffer = bytes.fromhex(hex_string)
@@ -42,27 +50,25 @@ class CartesifyBackend:
 
                 resp = requests.request(cartesify_data)
 
-                return True
-            return False
+                return "accept"
+            return "reject"
 
         except Exception as e:
             print(e)
             print("Sending reject")
-            error_message = e.args[0] if len(e.args) > 0 else "Unexpected Error"
-            error_json = json.dumps({"error": {"message": error_message}})
-            buffer = bytes(error_json, "utf8")
-            hex_payload = "0x" + buffer.hex()
-            rollup.report(hex_payload)
-            return False
+            # error_message = e.args[0] if len(e.args) > 0 else "Unexpected Error"
+            # error_json = json.dumps({"error": {"message": error_message}})
+            # buffer = bytes(error_json, "utf8")
+            # hex_payload = "0x" + buffer.hex()
+            # rollup.report(hex_payload)
+            return "reject"
 
-    @dapp_advance_decorator
-    def handle_advance(self, rollup:Rollup, data: RollupData) -> bool:
-        logger.info("Cartesify handle advance                                       ")
-
+    def handle_advance(self, data):
+        logger.info("Cartesify handle advance")
+        payload = data['payload']
         try:
-            payload = data.str_payload()
             if not payload.startswith('0x7b22'):
-                return False
+                return "reject"
 
             hex_string = payload[2:]  # Remove o prefixo '0x'
             buffer = bytes.fromhex(hex_string)
@@ -75,7 +81,9 @@ class CartesifyBackend:
             if 'cartesify' in json_data:
                 cartesify_data = json_data['cartesify']
 
-                resp = requests.request(cartesify_data)
+                logger.info(f'Cartesify Data {cartesify_data}')
+
+                resp = requests.request(url=cartesify_data['url'], method=cartesify_data['method'], headers=cartesify_data['headers'], json=cartesify_data['body'])
 
                 json_string = json.dumps({
                     'success': {
@@ -91,21 +99,21 @@ class CartesifyBackend:
                 # Converta para hexadecimal
                 hex_payload = "0x" + byte_buffer.hex()
 
-                rollup.report(hex_payload)
+                #rollup.report(hex_payload)
 
-                return True
+                return "accept"
 
-            return False
+            return "reject"
 
         except Exception as e:
             print(e)
             print("Sending reject")
-            error_message = e.args[0] if len(e.args) > 0 else "Unexpected Error"
-            error_json = json.dumps({"error": {"message": error_message}})
-            buffer = bytes(error_json, "utf8")
-            hex_payload = "0x" + buffer.hex()
-            rollup.report(hex_payload)
-            return False
+            # error_message = e.args[0] if len(e.args) > 0 else "Unexpected Error"
+            # error_json = json.dumps({"error": {"message": error_message}})
+            # buffer = bytes(error_json, "utf8")
+            # hex_payload = "0x" + buffer.hex()
+            # rollup.report(hex_payload)
+            return "reject"
 
 
 
